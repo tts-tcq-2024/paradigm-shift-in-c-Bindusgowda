05d19536f2f18bb797ce678771d2236a6d52a68f
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-10-08 14:26:56 +0530

    Create Parameter_Check.h

---
+++ Parameter_Check.h
@@ -0,0 +1,17 @@
+#include "Battery_Mon.h"
+
+#ifndef PARAMETER_CHECK_H
+#define PARAMETER_CHECK_H
+
+typedef struct {
+    const char *errorMessage;
+    const char *warningLow;
+    const char *warningHigh;
+} ParameterState;
+
+void setParameterState(ParameterState *state, const char *errorMessage, const char *warningLow, const char *warningHigh);
+void printMessage(const char *message);
+void checkBounds(float value, float min, float max, const char *errorMessage);
+void checkWarnings(float value, float min, float max, const ParameterState *state);
+
+#endif
