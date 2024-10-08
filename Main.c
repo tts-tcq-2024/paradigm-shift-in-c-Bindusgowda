ddbbe3caacd0f90022616c547312f24ea82c3164
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-10-08 14:08:10 +0530

    Update and rename checker.c to Main.c

---
+++ Main.c
@@ -0,0 +1,21 @@
+#include "parameter_state.h"
+#include "battery_monitor.h"
+
+int main() {
+    ParameterState temperatureState;
+    ParameterState socState;
+    ParameterState chargeRateState;
+
+    checkTemperature(46, &temperatureState); 
+    checkTemperature(39, &temperatureState); 
+    checkTemperature(25, &temperatureState); 
+
+    checkSoc(85, &socState); 
+    checkSoc(76, &socState); 
+    checkSoc(50, &socState); 
+
+    checkChargeRate(0.85, &chargeRateState); 
+    checkChargeRate(0.77, &chargeRateState); 
+    checkChargeRate(0.70, &chargeRateState); 
+    return 0;
+}
