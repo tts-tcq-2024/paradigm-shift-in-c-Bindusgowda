d9eb3ac61920ab33cb26327c9e776795202c647a
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-10-10 12:06:55 +0530

    Create BatteryMon_CheckSoc.c

---
+++ BatteryMon_CheckSoc.c
@@ -0,0 +1,21 @@
+#include "Battery_Mon.h"
+#include "Parameter_Check.h"
+
+void printMessage(const char *message)
+{
+    printf("%s\n", message);
+}
+
+void checkSoc(float soc, ParameterState *state) 
+{
+    setParameterState(state, "State of Charge out of range!", 
+                     "Warning: Approaching discharge!", 
+                     "Warning: Approaching charge-peak!");
+    
+    checkBounds(soc, SOC_MIN, SOC_MAX, state->errorMessage);
+    
+    if (WARN_FOR_SOC) 
+    {
+        checkWarnings(soc, SOC_MIN, SOC_MAX, state);
+    }
+}
