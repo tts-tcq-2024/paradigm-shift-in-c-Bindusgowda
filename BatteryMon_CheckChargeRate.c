4f06fd3660b182632937f7b6f3a5251badb57c5c
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-10-10 12:07:47 +0530

    Create BatteryMon_CheckChargeRate.c

---
+++ BatteryMon_CheckChargeRate.c
@@ -0,0 +1,21 @@
+#include "Battery_Mon.h"
+#include "Parameter_Check.h"
+
+void printMessage(const char *message)
+{
+    printf("%s\n", message);
+}
+
+void checkChargeRate(float chargeRate, ParameterState *state) 
+{
+    setParameterState(state, "Charge Rate out of range!", 
+                     "", 
+                     "Warning: Approaching charge rate peak!");
+    
+    checkBounds(chargeRate, 0, CHARGE_RATE_MAX, state->errorMessage);
+    
+    if (WARN_FOR_CHARGE_RATE) 
+    {
+        handleWarningHigh(chargeRate, CHARGE_RATE_MAX, state);
+    }
+}
