665ff86b9aca3176ef6b128942d489c8bebc73ec
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-10-10 12:26:24 +0530

    Update and rename Battery_Mon.h to Battery_Mon_Chech_Temperature.h

---
+++ Battery_Mon_Chech_Temperature.h
@@ -0,0 +1,16 @@
+#ifndef BATTERY_MON_CHECK_TEMPERATURE_H
+#define BATTERY_MON_CHECK_TEMPERATURE_H
+
+#include <stdio.h>
+#include "Parameter_Check.h"
+
+#define TEMPERATURE_MIN 0.0
+#define TEMPERATURE_MAX 45.0
+
+#define WARN_FOR_TEMPERATURE 1
+
+void printMessage(const char *message);
+
+void checkTemperature(float temperature, ParameterState *state);
+
+#endif 
